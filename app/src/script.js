8bbccdbcbcabed071cc4f15bc73b78c56c2465e9
mackele <larsmarcus.lennartsson@gmail.com>
2022-12-08 13:06:41 +0100

    Lite småändringar

--- app/src/script.js
+++ app/src/script.js
@@ -1,7 +1,7 @@
 
-function displayVisualisation() {
+function displayVisualisation1() {
     const container = document.getElementById("visualisation"), 
-    url = "https://public.tableau.com/views/Test_16702456908900/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    url = "https://public.tableau.com/views/Piechart_16705004681570/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
     settings = {
         hideTabs: true,
         onFirstInteractive: function () {
@@ -9,3 +9,13 @@ function displayVisualisation() {
     };
     var display = new tableau.Viz(container, url, settings); 
 }
+
+function displayVisualisation2() {
+    const container = document.getElementById("visualisation"), 
+    url = "https://public.tableau.com/views/Piechart_16705004681570/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    settings = {
+        hideTabs: true,
+        onFirstInteractive: function () {
+        }
+    };
+    var display = new tableau.Viz(container, url, settings); 
